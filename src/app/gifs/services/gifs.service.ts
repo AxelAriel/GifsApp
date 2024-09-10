7a3062f2190aff10b49ccab042d3326246846b61
Axel Vergara <axel.vergara94@gmail.com>
2024-09-10 03:10:34 -0300

    Save localStorage and readLocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -8,7 +8,6 @@ import { SearchResponse, Gif } from '../interfaces/gifs.interfaces';
 })
 
 
-
 export class GifsService {
 
   public gifsList: Gif[] = [];
@@ -18,7 +17,10 @@ export class GifsService {
   private serviceUrl:     string = 'https://api.giphy.com/v1/gifs';
   
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient) { 
+    this.loadLocalStorage();
+    // this.searchTag(this._tagsHistory.length);
+  }
   
   get tagsHistory() {    
     return [...this._tagsHistory];
@@ -33,13 +35,28 @@ export class GifsService {
     this._tagsHistory.unshift( tag );
     // console.log(this.tagHistory);
     this._tagsHistory = this.tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+  }
+
+  private loadLocalStorage():void{
+    if( !localStorage.getItem('history') ) return;
+
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+    
+    if( this._tagsHistory.length === 0 ) return;
+    this.searchTag( this._tagsHistory[0] );
+
 
   }
-  
+
   searchTag( tag: string ):void {
     
     if( tag.length === 0 ) return;
-    this.organizeHistory(tag);
+    this.organizeHistory( tag );
 
     const params = new HttpParams()
     .set( 'api_key', this.GIPHY_API_KEY )
@@ -50,7 +67,7 @@ export class GifsService {
     .subscribe( resp => {
 
       this.gifsList = resp.data;
-      // console.log( { gifs : this.gifsList } );
+      console.log( { gifs : this.gifsList } );
        
     } )        
   }
