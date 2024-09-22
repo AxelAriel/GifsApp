ccf6708963854083cf3e30b9616cb32df6ba4b4e
Axel Vergara <axel.vergara94@gmail.com>
2024-09-22 03:53:30 -0300

    fin seccion 07 appGifs

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from 'src/app/gifs/interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html'
+})
+export class LazyImageComponent implements OnInit {
+  
+  
+  @Input() 
+  public url!: string;
+  
+  @Input()
+  public alt!: string;
+
+  public hasLoaded: boolean = false;
+  
+  ngOnInit(): void {
+    if( !this.url ) throw new Error('Url property is required.');
+
+  }
+
+  onLoad() {
+    setTimeout(() => {
+      this.hasLoaded = true;
+    }, 1000);
+    
+  }
+
+}
